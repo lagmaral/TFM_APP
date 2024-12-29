1fe95a0381e10db954cd7006186c2f98a39779d2
lagmaral <lagmaral@uoc.edu>
2024-12-29 09:51:13 +0100

    Funcionalidades completas

--- src/app/resultados/components/resultados-home/resultados-home.component.ts
+++ src/app/resultados/components/resultados-home/resultados-home.component.ts
@@ -13,32 +13,7 @@ import { DatePipe } from '@angular/common';
 export class ResultadosHomeComponent  implements OnInit {
 
   partidoList:PartidoDTO[] = []
-  items = [
-    { title: 'Card 1', content: 'This is the content for Card 1' },
-    { title: 'Card 2', content: 'This is the content for Card 2' },
-    { title: 'Card 3', content: 'This is the content for Card 3' },
-    { title: 'Card 1', content: 'This is the content for Card 1' },
-    { title: 'Card 2', content: 'This is the content for Card 2' },
-    { title: 'Card 3', content: 'This is the content for Card 3' },
-    { title: 'Card 1', content: 'This is the content for Card 1' },
-    { title: 'Card 2', content: 'This is the content for Card 2' },
-    { title: 'Card 3', content: 'This is the content for Card 3' },
-    { title: 'Card 1', content: 'This is the content for Card 1' },
-    { title: 'Card 2', content: 'This is the content for Card 2' },
-    { title: 'Card 3', content: 'This is the content for Card 3' },
-    { title: 'Card 1', content: 'This is the content for Card 1' },
-    { title: 'Card 2', content: 'This is the content for Card 2' },
-    { title: 'Card 3', content: 'This is the content for Card 3' },
-    { title: 'Card 1', content: 'This is the content for Card 1' },
-    { title: 'Card 2', content: 'This is the content for Card 2' },
-    { title: 'Card 3', content: 'This is the content for Card 3' },
-    // Añade más elementos según sea necesario
-  ];
-
   matches:Match[] = [];
-
-
-  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
 
   constructor(private store: Store<AppState>,
     private datePipe: DatePipe
@@ -54,36 +29,10 @@ export class ResultadosHomeComponent  implements OnInit {
       this.partidoList = partido.partidosList;
       this.matches = Array.from(this.partidoList , (item) => this.transformToMatch(item));
 
-      //this.teamList = team.teamList;
-      /*this.cards = team.teamList.map((item: EquipoDTO) => ({
-        id:item.id,
-        image: {
-          default: item.internalkey+'-1200.webp',
-          srcset: `
-          http://localhost:3000${item.internalkey}-400.webp 400w,
-          http://localhost:3000${item.internalkey}-800.webp 800w,
-          http://localhost:3000${item.internalkey}-1200.webp 1200w
-        `,
-        sizes: '(max-width: 400px) 100vw, (max-width: 800px) 50vw, 33vw',
-        },
-        nombre: item.nombre,
-        categoria: item.descripcion,
-      }));
-      this.updateVisibleCards();*/
     });
-    // Forzar detección de cambios
-    //this.cd.detectChanges();*/
+
   }
 
-  scrollLeft() {
-    const element = this.scrollContainer.nativeElement as HTMLElement;
-    element.scrollBy({ left: -200, behavior: 'smooth' }); // Ajusta -200 según el ancho de las tarjetas
-  }
-
-  scrollRight() {
-    const element = this.scrollContainer.nativeElement as HTMLElement;
-    element.scrollBy({ left: 200, behavior: 'smooth' }); // Ajusta 200 según el ancho de las tarjetas
-  }
 
   transformToMatch(data: PartidoDTO): Match {
 
@@ -100,7 +49,7 @@ export class ResultadosHomeComponent  implements OnInit {
           name: data.rival.nombre,
           score: data.golesvisitante ? data.golesvisitante+'' : ' - '
         },
-        date: this.datePipe.transform(data.fecha, 'dd/MM/yyyy HH:mm:ss') || ''//data.date || new Date().toISOString()
+        date: this.datePipe.transform(data.fecha, 'dd/MM/yyyy HH:mm') || ''//data.date || new Date().toISOString()
       };
     }else{
       return {
@@ -115,7 +64,7 @@ export class ResultadosHomeComponent  implements OnInit {
           name: data.equipo.nombre+' '+data.equipo.descripcion,
           score: data.golesvisitante ? data.golesvisitante+'' : ' - '
         },
-        date: this.datePipe.transform(data.fecha, 'dd/MM/yyyy HH:mm:ss') || ''//data.date || new Date().toISOString()
+        date: this.datePipe.transform(data.fecha, 'dd/MM/yyyy HH:mm') || ''//data.date || new Date().toISOString()
       };
     }
 
